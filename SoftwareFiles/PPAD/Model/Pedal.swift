02870aae7643b9c9549cd3eb202e51b6a02b9429
Lucas <77511972+lucaslaya@users.noreply.github.com>
2024-01-02 18:13:53 -0300

    Added abstracted functionality to improve code

--- SoftwareFiles/PPAD/Model/Pedal.swift
+++ SoftwareFiles/PPAD/Model/Pedal.swift
@@ -12,5 +12,37 @@ struct Pedal {
     var angle: Double
     var type: Int
     var hold: Bool
-    var state: Bool
+    var state: Bool //Is on or not
+    
+    var holdOn = false
+    var resetHold = false
+    
+    var isAbove: Bool = false
+    
+    // Add threshold detection method
+    mutating func isAboveThreshold(highValue: Double, lowValue: Double) -> Bool {
+        if state == true {
+            let currentValue = inverse ? (highValue < lowValue) : (highValue > lowValue)
+            
+            // When hold functionality is enabled
+            if hold {
+                if !holdOn && currentValue {
+                    isAbove = true
+                    holdOn = true // First time crossing the threshold
+                } else if holdOn && !currentValue {
+                    resetHold = true // Reset hold when it goes below the threshold
+                } else if resetHold && currentValue {
+                    isAbove = !isAbove // Toggle isAbove when crossing the threshold again
+                    resetHold = false // Reset the resetHold flag
+                }
+            } else {
+                // Default behavior without hold functionality
+                isAbove = currentValue
+            }
+            
+            return isAbove
+        } else {
+            return false
+        }
+    }
 }
